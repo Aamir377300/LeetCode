b958fbc6b226e33ccb5517113c90fc70153cfb01
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-09 11:54:27 +0530

    Time: 0 ms (100%), Space: 44.2 MB (9.83%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,21 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null){
+
+        /*
+        VISUALIZATION OF RECURSION
+        ---------------------------
+
+        Given Tree (root = [3,9,20,null,null,15,7]):
+
+                3
+               / \
+              9   20
+                 /  \
+                15   7
+
+        CALL STACK (going DOWN)
+        maxDepth(3)
+            maxDepth(9) -> \U0001f9e0 main left of the maxDepth(3)
+                maxDepth(null) -> 0(left)
+                maxDepth(null) -> 0(right)
+            return max(0,0) + 1 = 1   (depth of node 9)
+
+            maxDepth(20) -> \U0001f9e0 main right of the maxDepth(3)
+                maxDepth(15)
+                    maxDepth(null) -> 0(left)
+                    maxDepth(null) -> 0(right)
+                return max(0,0) + 1 = 1   (depth of node 15)
+
+                maxDepth(7)
+                    maxDepth(null) -> 0(left)
+                    maxDepth(null) -> 0(right)
+                return max(0,0) + 1 = 1   (depth of node 7)
+
+            return max(1,1) + 1 = 2   (depth of node 20)
+
+        // the left of the 3(root) is the 1 (depth)
+        // the right of the 3(root) is the 2 (depth)
+
+        //  Math.max(leftHt, rightHt) + 1; --> give us the 3
+
+
+        Final Output = 3
+        */
+
+        if (root == null) {
             return 0;
         }
 
