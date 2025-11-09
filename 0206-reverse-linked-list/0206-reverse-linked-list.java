f8b4fedbe95f362f7a3a2e4527a34d1f0e39c6ee
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-09 11:33:39 +0530

    Time: 0 ms (100%), Space: 44.4 MB (6.67%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -61,16 +61,61 @@ import java.util.*;
 // recurrsion
 class Solution {
     public ListNode reverseList(ListNode head) {
-    if (head == null || head.next == null)
-        return head;
 
+        /*
+        VISUALIZATION OF THE RECURSION
+        --------------------------------
 
+        Example list:
+        1 -> 2 -> 3 -> 4 -> 5 -> null
 
-    ListNode newHead = reverseList(head.next);
+        CALL STACK (going DOWN)
+        reverseList(1)
+           reverseList(2)
+              reverseList(3)
+                 reverseList(4)
+                    reverseList(5)
+                       reverseList(null) -> return null
 
-    head.next.next = head;
-    head.next = null;
+        UNWINDING (coming UP)
 
-    return newHead;
+        Returning from reverseList(5):
+            newHead = 5 // as 5 is alone so directly it is linked list so its next become null
+            List: 5 -> null
+
+        Returning from reverseList(4):
+            4.next.next = 4   (5 -> 4){ys moment pr 4 ka next tha 5}
+            4.next = null
+            List: 5 -> 4 -> null
+
+        Returning from reverseList(3):
+            3.next.next = 3   (4 -> 3)
+            3.next = null
+            List: 5 -> 4 -> 3 -> null
+
+        Returning from reverseList(2):
+            2.next.next = 2   (3 -> 2)
+            2.next = null
+            List: 5 -> 4 -> 3 -> 2 -> null
+
+        Returning from reverseList(1):
+            1.next.next = 1   (2 -> 1)
+            1.next = null
+            FINAL LIST:
+            5 -> 4 -> 3 -> 2 -> 1 -> null
+        */
+
+        // BASE CASE
+        if (head == null || head.next == null)
+            return head;
+
+        // RECURSIVE CALL
+        ListNode newHead = reverseList(head.next);
+
+        // REVERSE THE POINTER
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
