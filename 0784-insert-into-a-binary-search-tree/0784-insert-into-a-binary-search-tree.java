197a8e339eae692de3546dd289d29d343cff8c9d
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-09 23:13:36 +0530

    Time: 0 ms (100%), Space: 47.3 MB (6.2%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -13,27 +13,57 @@
  *     }
  * }
  */
+ // recurrsive approach
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if (root == null) {
+//             return new TreeNode(val);
+//         }
+
+//         if (val < root.val) {
+//             if (root.left == null) {
+//                 root.left = new TreeNode(val);
+//                 // agar left node ke left null nahi hai thenn we do insertIntoBST for left so it is tranverse.
+//             } else {
+//                 insertIntoBST(root.left, val);
+//             }
+//         } else {
+//             if (root.right == null) {
+//                 root.right = new TreeNode(val);
+//             } else {
+//                 insertIntoBST(root.right, val);
+//             }
+//         }
+
+//         return root;
+//     }
+// }
+
+
+// iteractive approach 
+
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if (root == null) {
-            return new TreeNode(val);
-        }
-
-        if (val < root.val) {
-            if (root.left == null) {
-                root.left = new TreeNode(val);
-                // agar left node ke left null nahi hai thenn we do insertIntoBST for left so it is tranverse.
-            } else {
-                insertIntoBST(root.left, val);
+        if(root == null) return new TreeNode(val);
+        
+        TreeNode curr = root;
+        
+        while(true){
+            if(curr.val < val){
+                if(curr.right != null) curr = curr.right;
+                else {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
             }
-        } else {
-            if (root.right == null) {
-                root.right = new TreeNode(val);
-            } else {
-                insertIntoBST(root.right, val);
+            else{
+                if(curr.left != null) curr = curr.left;
+                else{
+                    curr.left = new TreeNode(val);
+                    break;
+                }
             }
         }
-
         return root;
     }
 }
