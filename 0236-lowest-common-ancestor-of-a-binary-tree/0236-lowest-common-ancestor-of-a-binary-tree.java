f09990e8a04fcee7195f16685a64d149a11a6ae4
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-19 00:10:38 +0530

    Time: 10 ms (12.15%), Space: 46.1 MB (26.84%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -11,24 +11,70 @@
 //  the depest ancenstor is known as the LCA 
 // https://www.youtube.com/watch?v=_-QHfMDde90
 
+// class Solution {
+
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+//         // BASE CASE
+//         if (root == null || root == p || root == q) {
+//             return root;
+//         }
+
+//         TreeNode left = lowestCommonAncestor(root.left, p, q);
+//         TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+//         if (left == null) {
+//             return right;
+//         } else if (right == null) {
+//             return left;   
+//         } else { // if both are null then we get our values
+//             return root;
+//         }
+//     }
+// }
+
+// Brute approach
+
+// 	1.	Find the path from root to p
+//	2.	Find the path from root to q
+//	3.	Compare both paths from the beginning
+//	4.	The last common element is the LCA
+
 class Solution {
 
+    // Method to find path from root to a given node
+    private boolean findPath(TreeNode root, TreeNode target, List<TreeNode> path) {
+        if (root == null) return false;
+
+        path.add(root);
+
+        if (root == target) return true;
+
+        if (findPath(root.left, target, path) || findPath(root.right, target, path))
+            return true;
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path1 = new ArrayList<>();
+        List<TreeNode> path2 = new ArrayList<>();
 
-        // BASE CASE
-        if (root == null || root == p || root == q) {
-            return root;
+        findPath(root, p, path1);
+        findPath(root, q, path2);
+
+        int i = 0;
+        TreeNode lca = null;
+
+        // Compare paths
+        while (i < path1.size() && i < path2.size()) {
+            if (path1.get(i) != path2.get(i))
+                break;
+            lca = path1.get(i);
+            i++;
         }
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if (left == null) {
-            return right;
-        } else if (right == null) {
-            return left;   
-        } else { // if both are null then we get our values
-            return root;
-        }
+        return lca;
     }
 }
