f88880fcc63a904e8479aac210e08d133b186b22
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-24 00:40:55 +0530

    Time: 3 ms (85.32%), Space: 52.5 MB (19.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,41 @@
-class Solution {
+// you only allow to move the vertical and horizontal of the particular element of the matrix
 
-    private void dfs(int i, int j, char[][] grid, int[][] vis, int m, int n) {
-        // boundary + visited + water check
-        if (i < 0 || i >= m || j < 0 || j >= n || vis[i][j] == 1 || grid[i][j] == '0') {
-            return;
+// https://drive.google.com/file/d/1G0xOmKh6HIn6fSvYfL4PZET4Vd_2k4MK/view?usp=sharing
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
         }
 
-        vis[i][j] = 1;
-
-        // explore in all 4 directions
-        dfs(i + 1, j, grid, vis, m, n);
-        dfs(i - 1, j, grid, vis, m, n);
-        dfs(i, j + 1, grid, vis, m, n);
-        dfs(i, j - 1, grid, vis, m, n);
-    }
-
-    public int numIslands(char[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[][] vis = new int[m][n];
         int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                // If it's land and not visited
-                if (vis[i][j] == 0 && grid[i][j] == '1') {
-                    dfs(i, j, grid, vis, m, n);
-                    count++;   // one island completed
+    // grid.length is define as the no of the rows
+        for (int i = 0; i < grid.length; i++){
+            // grid.length[0] is define as the no of the coloumns
+            for (int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
                 }
             }
         }
 
         return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length
+                || j < 0 || j >= grid[0].length
+                || grid[i][j] == '0') {
+            return;
+        }
+        // we do not create a visited matrix or list whoever we moved we just replace them with the 0(here 1 is replace by the 0)
+        grid[i][j] = '0'; // Mark the cell as visited
+
+        // Explore all four directions
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
