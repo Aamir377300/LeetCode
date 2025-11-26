8563ec55c0cd50d4a51fe3397c27935a34c62b08
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-11-26 16:48:56 +0530

    Time: 69 ms (57.66%), Space: 90.4 MB (47.75%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        int MOD = 1_000_000_007;
+        
+        // dp[i][j][r] = ways to reach (i,j) with sum % k = r
+        int[][][] dp = new int[m][n][k];
+        
+        // Base case: starting cell
+        dp[0][0][grid[0][0] % k] = 1;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int r = 0; r < k; r++) {
+                    if (dp[i][j][r] == 0) continue;
+                    
+                    int newR;
+                    
+                    // Move down
+                    if (i + 1 < m) {
+                        newR = (r + grid[i+1][j]) % k;
+                        dp[i+1][j][newR] = (dp[i+1][j][newR] + dp[i][j][r]) % MOD;
+                    }
+                    
+                    // Move right
+                    if (j + 1 < n) {
+                        newR = (r + grid[i][j+1]) % k;
+                        dp[i][j+1][newR] = (dp[i][j+1][newR] + dp[i][j][r]) % MOD;
+                    }
+                }
+            }
+        }
+        
+        // Answer: ways to reach bottom-right with remainder 0
+        return dp[m-1][n-1][0];
+    }
+}
