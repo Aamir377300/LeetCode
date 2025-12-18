b4dfe3ca1923eb159290f085ed8a08484f69dfcf
Aamir Belal Khan <belalaamirkhan@gmail.com>
2025-12-18 16:25:56 +0530

    Time: 0 ms (100%), Space: 42.4 MB (6.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,20 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         // Base cases
+//         if (n == 1) return 1;
+//         if (n == 2) return 2;
+
+//         // Recursive case
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// }
+
+// It is solve(run) but on submit, it give us the Time limit exceed
+
+// ----------------------------
+
+// using the Iterative DP
+
 class Solution {
     public int climbStairs(int n) {
 
