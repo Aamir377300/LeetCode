9722d4614a813e98af24d4c4daf36ce61a485a9f
Aamir Belal Khan <belalaamirkhan@gmail.com>
2026-02-08 23:17:13 +0530

    Time: 0 ms (100%), Space: 45.6 MB (57.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,6 +15,36 @@
  */
 
  // balanced subtree is defined as for every node |height of left subtree - height of right subtree| <= 1; // 1 se jada nahi hona chaiye
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         int ans = Height(root);
+
+//         if(ans == -1){
+//             return false;
+//         }
+//         else{
+//             return true;
+//         }
+        
+//     }
+
+//     public int Height(TreeNode root) {
+//         if (root == null) return 0;
+
+//         int leftHt = Height(root.left);
+//         if (leftHt == -1) return -1; // \U0001f448 it helps for some edges cases
+
+//         int rightHt = Height(root.right);
+//         if (rightHt == -1) return -1; // \U0001f448 it helps for some edges cases
+
+//         if (Math.abs(leftHt - rightHt) > 1) return -1;
+
+//         return Math.max(leftHt, rightHt) + 1; // we need to return the height because it is check above, this is recurrsion so it is going to use in the above when the recurrsion take place.
+//     }
+// }
+
+
+ // balanced subtree is defined as for every node |height of left subtree - height of right subtree| <= 1; // 1 se jada nahi hona chaiye
 class Solution {
     public boolean isBalanced(TreeNode root) {
         int ans = Height(root);
@@ -28,8 +58,8 @@ class Solution {
         
     }
 
-    public boolean Height(TreeNode root) {
-        if (root == null) return false;
+    public int Height(TreeNode root) {
+        if (root == null) return 0;
 
         int leftHt = Height(root.left);
         if (leftHt == -1) return -1; // \U0001f448 it helps for some edges cases
