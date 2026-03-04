73709df4c8386b309b3f8dc3b8cd5ac67bad8d0b
Aamir Belal Khan <belalaamirkhan@gmail.com>
2026-03-04 12:47:55 +0530

    Time: 0 ms (100%), Space: 43.9 MB (94.94%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,18 @@
-import java.util.*;
-
+// https://drive.google.com/file/d/1E7W6zQ69q0IqZz0le4Xdo7EQsXboaezb/view?usp=sharing
 class Solution {
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        List<Integer> current = new ArrayList<>();
-        backtrack(0, nums, current, result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), res);
+        return res;
     }
+    public void backtrack(int start, int[] nums, List<Integer> temp, List<List<Integer>> res){
+        // there is no need of the base condition kuki for loop me condition wala parameter i ko kabhi nums ke length se jaada jane dega nahi so proper control rahega
+        res.add(new ArrayList<>(temp));
 
-    private void backtrack(int index,int[] nums, List<Integer> current,List<List<Integer>> result) {
-        // Save the current subset
-        result.add(new ArrayList<>(current));
-
-        for (int i = index; i < nums.length; i++) {
-
-            current.add(nums[i]);              // choose
-            backtrack(i + 1, nums, current, result); // explore
-
-            // to remove the element frm current in the particular iteration
-            current.remove(current.size() - 1); // un-choose
+        for(int i = start; i<nums.length; i++){
+            temp.add(nums[i]);
+            backtrack(i+1, nums, temp, res);
+            temp.remove(temp.size() - 1);
         }
     }
 }
